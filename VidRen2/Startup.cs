931b5f2695d8a293a9e08274f0809ee36803d9e3
Vipul Jadhav <jvipul50@yahoo.com>
2019-07-10 21:01:26 -0400

    Add project files.

---
+++ VidRen2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VidRen2.Startup))]
+namespace VidRen2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
